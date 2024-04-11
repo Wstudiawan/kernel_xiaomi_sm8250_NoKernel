34582745ddb7f91ae1b0f8badcadfe988ceb5bc3
Wahyu Studiawan <manusiasampah7@gmail.com>
2024-04-11 19:32:28 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -79,7 +79,7 @@ FINAL_ZIP=${ZIPNAME}-${VERSION}-${DEVICE}-BETA2-KERNEL-AOSP-${TM}.zip
 FINAL_ZIP_AOSPA=${ZIPNAME}-${VERSION}-AOSPA-${DEVICE}-BETA2-KERNEL-AOSP-${TM}.zip
 
 # Specify compiler [ proton, nexus, aosp ]
-COMPILER=eva-gcc
+COMPILER=proton
 
 # Clone ToolChain
 function cloneTC() {
