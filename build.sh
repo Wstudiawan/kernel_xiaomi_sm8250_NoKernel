56c2d9ee0eb0e15c68cda8c25523e6ac70351cf0
Wahyu Studiawan <manusiasampah7@gmail.com>
2024-04-11 19:23:35 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
- #!/usr/bin/env bash
+#!/usr/bin/env bash
 
  #
  # Script For Building Android Kernel
@@ -6,6 +6,9 @@
 
 # Specify Kernel Directory
 KERNEL_DIR="$(pwd)"
+
+# Default linker to use for builds
+export LINKER="ld.lld"
 
 BUILD=$1
 
@@ -29,24 +32,34 @@ DEVICE=$2
 
 VERSION=BETA
 if [ "${DEVICE}" = "alioth" ]; then
-DEFCONFIG=alioth_defconfig
+DEFCONFIG=vendor/xiaomi/alioth.config
 MODEL="Poco F3"
 elif [ "${DEVICE}" = "lmi" ]; then
-DEFCONFIG=lmi_defconfig
+DEFCONFIG=vendor/xiaomi/lmi.config
 MODEL="Poco F2 Pro"
 elif [ "${DEVICE}" = "apollo" ]; then
-DEFCONFIG=apollo_defconfig
+DEFCONFIG=vendor/xiaomi/apollo.config
 MODEL="Mi 10T Pro"
 elif [ "${DEVICE}" = "munch" ]; then
-DEFCONFIG=vendor/munch_defconfig
+DEFCONFIG=vendor/xiaomi/munch.config
 MODEL="Poco F4"
+elif [ "${DEVICE}" = "cas" ]; then
+DEFCONFIG=vendor/xiaomi/cas.config
+MODEL="Mi 10 Ultra"
+elif [ "${DEVICE}" = "cmi" ]; then
+DEFCONFIG=vendor/xiaomi/cmi.config
+MODEL="Mi 10 Pro"
+elif [ "${DEVICE}" = "umi" ]; then
+DEFCONFIG=vendor/xiaomi/umi.config
+MODEL="Mi 10"
 fi
 
 # Files
 IMAGE=$(pwd)/out/arch/arm64/boot/Image
 DTBO=$(pwd)/out/arch/arm64/boot/dtbo.img
+DTB=$(pwd)/out/arch/arm64/boot/dtb.img
 OUT_DIR=out/
-dts_source=arch/arm64/boot/dts/vendor/qcom
+#dts_source=arch/arm64/boot/dts/vendor/qcom
 
 # Verbose Build
 VERBOSE=0
@@ -61,19 +74,12 @@ DATE=$(TZ=Europe/Lisbon date +"%Y%m%d-%T")
 TM=$(date +"%F%S")
 
 # Specify Final Zip Name
-ZIPNAME=Klinker
-FINAL_ZIP=${ZIPNAME}-${VERSION}-${DEVICE}-Klinker-KERNEL-MIUI-${TM}.zip
+ZIPNAME=Nexus
+FINAL_ZIP=${ZIPNAME}-${VERSION}-${DEVICE}-BETA2-KERNEL-AOSP-${TM}.zip
+FINAL_ZIP_AOSPA=${ZIPNAME}-${VERSION}-AOSPA-${DEVICE}-BETA2-KERNEL-AOSP-${TM}.zip
 
 # Specify compiler [ proton, nexus, aosp ]
-COMPILER=aosp
-
-# KernelSU
-git config --global user.email "manusiasampah7@gmail.com"
-git config --global user.name "Wstudiawan"
-curl https://gist.githubusercontent.com/bagaskara815/5aeb07f0d9031189871ffa362591b20f/raw/ksu.patch >> ksu.patch
-git am ksu.patch
-curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s main
-
+COMPILER=eva-gcc
 
 # Clone ToolChain
 function cloneTC() {
@@ -130,32 +136,45 @@ function cloneTC() {
 		    if [ ! -d clang ]; then
 				mkdir clang
             	cd clang
-		    	wget https://raw.githubusercontent.com/ZyCromerZ/Clang/main/Clang-16-lastbuild.txt
-		    	V="$(cat Clang-16-lastbuild.txt)"
-            	wget -q https://github.com/ZyCromerZ/Clang/releases/download/16.0.3-$V-release/Clang-16.0.3-$V.tar.gz
-	        	tar -xf Clang-16.0.3-$V.tar.gz
+		    	wget https://raw.githubusercontent.com/ZyCromerZ/Clang/main/Clang-main-lastbuild.txt
+		    	V="$(cat Clang-main-lastbuild.txt)"
+            	wget -q https://github.com/ZyCromerZ/Clang/releases/download/19.0.0git-$V-release/Clang-19.0.0git-$V.tar.gz
+	        	tar -xf Clang-19.0.0git-$V.tar.gz
 	        	cd ..
 				fi
 	        	PATH="${KERNEL_DIR}/clang/bin:$PATH"
 	        ;;
-
+	    slim)
+	        git clone --depth=1 https://gitlab.com/ThankYouMario/android_prebuilts_clang-standalone -b slim-16 clangB
+	        git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git --depth=1 gcc
+			git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git  --depth=1 gcc32
+			PATH="${KERNEL_DIR}/clangB/bin:${KERNEL_DIR}/gcc/bin:${KERNEL_DIR}/gcc32/bin:${PATH}"
+	        ;;
+	    eva-gcc)
+	        git clone https://github.com/mvaisakh/gcc-arm64 --depth=1 gcc64
+	        git clone https://github.com/mvaisakh/gcc-arm --depth=1 gcc32
+            PATH="${KERNEL_DIR}"/gcc32/bin:"${KERNEL_DIR}"/gcc64/bin:/usr/bin/:${PATH}
+            ;;
 		*)
 			echo "Compiler not defined"
 			;;
 	esac
         # Clone AnyKernel
-        if [ -d AnyKernel3 ]; then
-		  rm -rf AnyKernel3
-		else
+		rm -rf AnyKernel3
 		if [ "${DEVICE}" = "alioth" ]; then
           git clone --depth=1 https://github.com/NotZeetaa/AnyKernel3 -b alioth AnyKernel3
         elif [ "${DEVICE}" = "apollo" ]; then
           git clone --depth=1 https://github.com/NotZeetaa/AnyKernel3 -b apollo AnyKernel3
         elif [ "${DEVICE}" = "munch" ]; then
           git clone --depth=1 https://github.com/NotZeetaa/AnyKernel3 -b munch AnyKernel3
+		elif [ "${DEVICE}" = "cas" ]; then
+          git clone --depth=1 https://github.com/NotZeetaa/AnyKernel3 -b cas AnyKernel3
+		elif [ "${DEVICE}" = "cmi" ]; then
+          git clone --depth=1 https://github.com/NotZeetaa/AnyKernel3 -b cmi AnyKernel3
+		elif [ "${DEVICE}" = "umi" ]; then
+          git clone --depth=1 https://github.com/NotZeetaa/AnyKernel3 -b umi AnyKernel3
 		else
 		  git clone --depth=1 https://github.com/NotZeetaa/AnyKernel3 -b lmi AnyKernel3
-		fi
 		fi
 	}
 	
@@ -179,8 +198,8 @@ function exports() {
         export SUBARCH=arm64
                
         # KBUILD HOST and USER
-        export KBUILD_BUILD_HOST=Ubuntu
-        export KBUILD_BUILD_USER="wahyu_studi"
+        export KBUILD_BUILD_HOST=ArchLinux
+        export KBUILD_BUILD_USER="wahyustudi"
         
         # CI
         if [ "$CI" ]
@@ -231,7 +250,7 @@ START=$(date +"%s")
 	# Compile
 	if [ -d ${KERNEL_DIR}/clang ];
 	   then
-           make O=out CC=clang ARCH=arm64 ${DEFCONFIG}
+           make O=out CC=clang ARCH=arm64 vendor/kona-perf_defconfig vendor/xiaomi/sm8250-common.config ${DEFCONFIG}
 		   if [ "$METHOD" = "lto" ]; then
 		     scripts/config --file ${OUT_DIR}/.config \
              -e LTO_CLANG \
@@ -246,21 +265,95 @@ START=$(date +"%s")
 	       V=$VERBOSE 2>&1 | tee error.log
 	elif [ -d ${KERNEL_DIR}/gcc64 ];
 	   then
-           make O=out ARCH=arm64 ${DEFCONFIG}
+           make O=out ARCH=arm64 vendor/kona-perf_defconfig vendor/xiaomi/sm8250-common.config ${DEFCONFIG}
+		   if [ "$METHOD" = "lto" ]; then
+		     scripts/config --file ${OUT_DIR}/.config \
+             -e CONFIG_LTO_GCC
+           fi
+	       make -kj$(nproc --all) O=out \
+	       	ARCH=arm64 \
+	       	CC=aarch64-elf-gcc \
+			LD="${KERNEL_DIR}/gcc64/bin/aarch64-elf-ld.lld" \
+			AR=llvm-ar \
+			NM=llvm-nm \
+			OBJCOPY=llvm-objcopy \
+			OBJDUMP=llvm-objdump \
+			OBJCOPY=llvm-objcopy \
+			OBJSIZE=llvm-size \
+			STRIP=llvm-strip \
+			CROSS_COMPILE=aarch64-elf- \
+			CROSS_COMPILE_COMPAT=arm-eabi- \
+			CC_COMPAT=arm-eabi-gcc \
+	       	V=$VERBOSE 2>&1 | tee error.log
+        elif [ -d ${KERNEL_DIR}/clangB ];
+           then
+           make O=out CC=clang ARCH=arm64 vendor/kona-perf_defconfig vendor/xiaomi/sm8250-common.config ${DEFCONFIG}
+		   if [ "$METHOD" = "lto" ]; then
+		     scripts/config --file ${OUT_DIR}/.config \
+             -e LTO_CLANG \
+             -d THINLTO
+           fi
+           make -kj$(nproc --all) O=out \
+	       ARCH=arm64 \
+	       LLVM=1 \
+	       LLVM_IAS=1 \
+	       CLANG_TRIPLE=aarch64-linux-gnu- \
+	       CROSS_COMPILE=aarch64-linux-android- \
+	       CROSS_COMPILE_COMPAT=arm-linux-androideabi- \
+	       V=$VERBOSE 2>&1 | tee error.log
+	fi
+	
+	# Verify Files
+	if ! [ -a "$IMAGE" ];
+	   then
+	       push "error.log" "Build Throws Errors"
+	       exit 1
+	fi
+	}
+	
+function compile_ksu() {
+START=$(date +"%s")
+	# Compile
+	if [ -d ${KERNEL_DIR}/clang ];
+	   then
+           make O=out CC=clang ARCH=arm64 vendor/kona-perf_defconfig vendor/xiaomi/sm8250-common.config ${DEFCONFIG}
+		   if [ "$METHOD" = "lto" ]; then
+		     scripts/config --file ${OUT_DIR}/.config \
+             -e LTO_CLANG \
+             -d THINLTO
+           fi
 	       make -kj$(nproc --all) O=out \
 	       ARCH=arm64 \
-	       CROSS_COMPILE_COMPAT=arm-eabi- \
-	       CROSS_COMPILE=aarch64-elf- \
-	       AR=llvm-ar \
-	       NM=llvm-nm \
-	       OBJCOPY=llvm-objcopy \
-	       OBJDUMP=llvm-objdump \
-	       STRIP=llvm-strip \
-	       OBJSIZE=llvm-size \
+	       LLVM=1 \
+	       LLVM_IAS=1 \
+	       CROSS_COMPILE=aarch64-linux-gnu- \
+	       CROSS_COMPILE_COMPAT=arm-linux-gnueabi- \
+	       V=$VERBOSE 2>&1 | tee error.log
+	elif [ -d ${KERNEL_DIR}/gcc64 ];
+	   then
+           make O=out ARCH=arm64 vendor/kona-perf_defconfig vendor/xiaomi/sm8250-common.config ${DEFCONFIG}
+	       if [ "$METHOD" = "lto" ]; then
+		     scripts/config --file ${OUT_DIR}/.config \
+             -e CONFIG_LTO_GCC
+           fi
+		   make -kj$(nproc --all) O=out \
+	       ARCH=arm64 \
+	       CC=aarch64-elf-gcc \
+			LD="${KERNEL_DIR}/gcc64/bin/aarch64-elf-ld.lld" \
+			AR=llvm-ar \
+			NM=llvm-nm \
+			OBJCOPY=llvm-objcopy \
+			OBJDUMP=llvm-objdump \
+			OBJCOPY=llvm-objcopy \
+			OBJSIZE=llvm-size \
+			STRIP=llvm-strip \
+			CROSS_COMPILE=aarch64-elf- \
+			CROSS_COMPILE_COMPAT=arm-eabi- \
+			CC_COMPAT=arm-eabi-gcc \
 	       V=$VERBOSE 2>&1 | tee error.log
         elif [ -d ${KERNEL_DIR}/clangB ];
            then
-           make O=out CC=clang ARCH=arm64 ${DEFCONFIG}
+           make O=out CC=clang ARCH=arm64 vendor/kona-perf_defconfig vendor/xiaomi/sm8250-common.config ${DEFCONFIG}
 		   if [ "$METHOD" = "lto" ]; then
 		     scripts/config --file ${OUT_DIR}/.config \
              -e LTO_CLANG \
@@ -283,30 +376,236 @@ START=$(date +"%s")
 	       exit 1
 	   else
 	       post_msg " Kernel Compilation Finished. Started Zipping "
-		   find ${OUT_DIR}/$dts_source -name '*.dtb' -exec cat {} + >${OUT_DIR}/arch/arm64/boot/dtb
-		   DTB=$(pwd)/out/arch/arm64/boot/dtb
 	fi
-	}
+}
 
 # Zipping
-function zipping() {
+function move() {
 	# Copy Files To AnyKernel3 Zip
 	mv $IMAGE AnyKernel3
     mv $DTBO AnyKernel3
     mv $DTB AnyKernel3
+}
 
-	# Zipping and Push Kernel
-	cd AnyKernel3 || exit 1
-        zip -r9 ${FINAL_ZIP} *
-        MD5CHECK=$(md5sum "$FINAL_ZIP" | cut -d' ' -f1)
-        push "$FINAL_ZIP" "Build took : $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s) | For <b>$MODEL ($DEVICE)</b> | <b>${KBUILD_COMPILER_STRING}</b> | <b>MD5 Checksum : </b><code>$MD5CHECK</code>"
-		cd ..
-        rm -rf AnyKernel3
-        }
+function move_ksu() {
+	mv $IMAGE AnyKernel3/ksu/
+}
+
+function zipping() {
+    # Zipping and Push Kernel
+    cd AnyKernel3 || exit 1
+    zip -r9 ${FINAL_ZIP} *
+    MD5CHECK=$(md5sum "$FINAL_ZIP" | cut -d' ' -f1)
+    UPLOAD_GOFILE=$(curl -F file=@$FINAL_ZIP https://store1.gofile.io/uploadFile)
+    DOWNLOAD_LINK_GOFILE=$(echo $UPLOAD_GOFILE | awk -F '"' '{print $10}')
+    push "$FINAL_ZIP" "Build took : $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s) | For <b>$MODEL ($DEVICE)</b> | <b>${KBUILD_COMPILER_STRING}</b> | <b>MD5 Checksum : </b><code>$MD5CHECK</code> | GOFILE Download link: $DOWNLOAD_LINK_GOFILE"
+    cd ..
+}
+
+function compile_aospa() {
+START=$(date +"%s")
+	# Push Notification
+	post_msg "<b>$KBUILD_BUILD_VERSION CI Build Triggered</b>%0A<b>Docker OS: </b><code>$DISTRO</code>%0A<b>Kernel Version : </b><code>$KERVER - AOSPA</code>%0A<b>Date : </b><code>$(TZ=Europe/Lisbon date)</code>%0A<b>Device : </b><code>$MODEL [$DEVICE]</code>%0A<b>Pipeline Host : </b><code>$KBUILD_BUILD_HOST</code>%0A<b>Host Core Count : </b><code>$PROCS</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0A<b>Branch : </b><code>$CI_BRANCH</code>%0A<b>Top Commit : </b><a href='$DRONE_COMMIT_LINK'>$COMMIT_HEAD</a>"
+	
+	# Compile
+	if [ -d ${KERNEL_DIR}/clang ];
+	   then
+           make O=out CC=clang ARCH=arm64 vendor/kona-perf_defconfig vendor/xiaomi/sm8250-common.config ${DEFCONFIG}
+		   if [ "$METHOD" = "lto" ]; then
+		     scripts/config --file ${OUT_DIR}/.config \
+             -e LTO_CLANG \
+             -d THINLTO
+           fi
+	       make -kj$(nproc --all) O=out \
+	       ARCH=arm64 \
+	       LLVM=1 \
+	       LLVM_IAS=1 \
+	       CROSS_COMPILE=aarch64-linux-gnu- \
+	       CROSS_COMPILE_COMPAT=arm-linux-gnueabi- \
+	       V=$VERBOSE 2>&1 | tee error.log
+	elif [ -d ${KERNEL_DIR}/gcc64 ];
+	   then
+           make O=out ARCH=arm64 vendor/kona-perf_defconfig vendor/xiaomi/sm8250-common.config ${DEFCONFIG}
+	       if [ "$METHOD" = "lto" ]; then
+		     scripts/config --file ${OUT_DIR}/.config \
+             -e CONFIG_LTO_GCC
+           fi
+		   make -kj$(nproc --all) O=out \
+	       ARCH=arm64 \
+	       CC=aarch64-elf-gcc \
+			LD="${KERNEL_DIR}/gcc64/bin/aarch64-elf-ld.lld" \
+			AR=llvm-ar \
+			NM=llvm-nm \
+			OBJCOPY=llvm-objcopy \
+			OBJDUMP=llvm-objdump \
+			OBJCOPY=llvm-objcopy \
+			OBJSIZE=llvm-size \
+			STRIP=llvm-strip \
+			CROSS_COMPILE=aarch64-elf- \
+			CROSS_COMPILE_COMPAT=arm-eabi- \
+			CC_COMPAT=arm-eabi-gcc \
+	       V=$VERBOSE 2>&1 | tee error.log
+        elif [ -d ${KERNEL_DIR}/clangB ];
+           then
+           make O=out CC=clang ARCH=arm64 vendor/kona-perf_defconfig vendor/xiaomi/sm8250-common.config ${DEFCONFIG}
+		   if [ "$METHOD" = "lto" ]; then
+		     scripts/config --file ${OUT_DIR}/.config \
+             -e LTO_CLANG \
+             -d THINLTO
+           fi
+           make -kj$(nproc --all) O=out \
+	       ARCH=arm64 \
+	       LLVM=1 \
+	       LLVM_IAS=1 \
+	       CLANG_TRIPLE=aarch64-linux-gnu- \
+	       CROSS_COMPILE=aarch64-linux-android- \
+	       CROSS_COMPILE_COMPAT=arm-linux-androideabi- \
+	       V=$VERBOSE 2>&1 | tee error.log
+	fi
+	
+	# Verify Files
+	if ! [ -a "$IMAGE" ];
+	   then
+	       push "error.log" "Build Throws Errors"
+	       exit 1
+	fi
+	}
+	
+function compile_ksu_aospa() {
+START=$(date +"%s")
+	# Compile
+	if [ -d ${KERNEL_DIR}/clang ];
+	   then
+           make O=out CC=clang ARCH=arm64 vendor/kona-perf_defconfig vendor/xiaomi/sm8250-common.config ${DEFCONFIG}
+		   if [ "$METHOD" = "lto" ]; then
+		     scripts/config --file ${OUT_DIR}/.config \
+             -e LTO_CLANG \
+             -d THINLTO
+           fi
+		   scripts/config --file ${OUT_DIR}/.config \
+    		-d SDCARD_FS \
+    		-e UNICODE \
+			-e F2FS_FS_COMPRESSION \
+			-e RD_LZ4
+	       make -kj$(nproc --all) O=out \
+	       ARCH=arm64 \
+	       LLVM=1 \
+	       LLVM_IAS=1 \
+	       CROSS_COMPILE=aarch64-linux-gnu- \
+	       CROSS_COMPILE_COMPAT=arm-linux-gnueabi- \
+	       V=$VERBOSE 2>&1 | tee error.log
+	elif [ -d ${KERNEL_DIR}/gcc64 ];
+	   then
+           make O=out ARCH=arm64 vendor/kona-perf_defconfig vendor/xiaomi/sm8250-common.config ${DEFCONFIG}
+	       if [ "$METHOD" = "lto" ]; then
+		     scripts/config --file ${OUT_DIR}/.config \
+             -e CONFIG_LTO_GCC
+           fi
+		   make -kj$(nproc --all) O=out \
+	       ARCH=arm64 \
+	       CC=aarch64-elf-gcc \
+			LD="${KERNEL_DIR}/gcc64/bin/aarch64-elf-ld.lld" \
+			AR=llvm-ar \
+			NM=llvm-nm \
+			OBJCOPY=llvm-objcopy \
+			OBJDUMP=llvm-objdump \
+			OBJCOPY=llvm-objcopy \
+			OBJSIZE=llvm-size \
+			STRIP=llvm-strip \
+			CROSS_COMPILE=aarch64-elf- \
+			CROSS_COMPILE_COMPAT=arm-eabi- \
+			CC_COMPAT=arm-eabi-gcc \
+	       V=$VERBOSE 2>&1 | tee error.log
+        elif [ -d ${KERNEL_DIR}/clangB ];
+           then
+           make O=out CC=clang ARCH=arm64 vendor/kona-perf_defconfig vendor/xiaomi/sm8250-common.config ${DEFCONFIG}
+		   if [ "$METHOD" = "lto" ]; then
+		     scripts/config --file ${OUT_DIR}/.config \
+             -e LTO_CLANG \
+             -d THINLTO
+           fi
+		   scripts/config --file ${OUT_DIR}/.config \
+    		-d SDCARD_FS \
+    		-e UNICODE \
+			-e F2FS_FS_COMPRESSION \
+			-e RD_LZ4
+           make -kj$(nproc --all) O=out \
+	       ARCH=arm64 \
+	       LLVM=1 \
+	       LLVM_IAS=1 \
+	       CLANG_TRIPLE=aarch64-linux-gnu- \
+	       CROSS_COMPILE=aarch64-linux-android- \
+	       CROSS_COMPILE_COMPAT=arm-linux-androideabi- \
+	       V=$VERBOSE 2>&1 | tee error.log
+	fi
+	
+	# Verify Files
+	if ! [ -a "$IMAGE" ];
+	   then
+	       push "error.log" "Build Throws Errors"
+	       exit 1
+	   else
+	       post_msg " Kernel Compilation Finished. Started Zipping "
+	fi
+}
+
+# Zipping
+function move_aospa() {
+	# Copy Files To AnyKernel3 Zip
+	mv $IMAGE AnyKernel3
+    mv $DTBO AnyKernel3
+    mv $DTB AnyKernel3
+}
+
+function move_ksu_aospa() {
+	mv $IMAGE AnyKernel3/ksu/
+}
+
+function zipping_aospa() {
+    # Zipping and Push Kernel
+    cd AnyKernel3 || exit 1
+    zip -r9 ${FINAL_ZIP_AOSPA} *
+    MD5CHECK=$(md5sum "$FINAL_ZIP_AOSPA" | cut -d' ' -f1)
+    UPLOAD_GOFILE=$(curl -F file=@$FINAL_ZIP_AOSPA https://store1.gofile.io/uploadFile)
+    DOWNLOAD_LINK_GOFILE=$(echo $UPLOAD_GOFILE | awk -F '"' '{print $10}')
+    push "$FINAL_ZIP_AOSPA" "Build took : $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s) | For <b>$MODEL ($DEVICE)</b> | <b>${KBUILD_COMPILER_STRING}</b> | <b>MD5 Checksum : </b><code>$MD5CHECK</code> | GOFILE Download link: $DOWNLOAD_LINK_GOFILE"
+    cd ..
+    rm -rf AnyKernel3
+}
 
 cloneTC
 exports
 compile
 END=$(date +"%s")
 DIFF=$(($END - $START))
+move
+# KernelSU
+echo "CONFIG_KSU=y" >> $(pwd)/arch/arm64/configs/$DEFCONFIG
+compile_ksu
+move_ksu
 zipping
+if [ "$BUILD" = "local" ]; then
+# Discard KSU changes in defconfig
+git restore arch/arm64/configs/$DEFCONFIG
+fi
+if [ "${DEVICE}" = "alioth" ]; then
+	compile_aospa
+	END=$(date +"%s")
+	DIFF=$(($END - $START))
+	move_aospa
+	# KernelSU
+	echo "CONFIG_KSU=y" >> $(pwd)/arch/arm64/configs/$DEFCONFIG
+	compile_ksu_aospa
+	move_ksu_aospa
+	zipping_aospa
+elif [ "${DEVICE}" = "munch" ]; then
+	compile_aospa
+	END=$(date +"%s")
+	DIFF=$(($END - $START))
+	move_aospa
+	# KernelSU
+	echo "CONFIG_KSU=y" >> $(pwd)/arch/arm64/configs/$DEFCONFIG
+	compile_ksu_aospa
+	move_ksu_aospa
+	zipping_aospa
+fi
